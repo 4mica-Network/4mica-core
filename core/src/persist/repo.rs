74c931dd828ea0d72048db69b0799b5f6f6debe2
Erik Takke <erik.takke@3milabs.tech>
2025-08-25 17:48:25 +0200

    Remove `submit_payment_transaction`

--- core/src/persist/repo.rs
+++ core/src/persist/repo.rs
@@ -4,14 +4,10 @@ use alloy::dyn_abi::DynSolValue;
 use alloy::primitives::{address, Address, TxHash, B256};
 use alloy::primitives::utils::format_units;
 use alloy::providers::fillers::{BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller};
-use alloy::providers::{Identity, Provider, RootProvider};
+use alloy::providers::{Identity, RootProvider};
 use alloy::sol;
 use futures_util::future::join_all;
-use crate::persist::prisma::{user, user_transaction};
-use crate::persist::PersistCtx;
-use prisma_client_rust::QueryError;
 use rpc::common::UserTransactionInfo;
-use thiserror::Error;
 use blockchain::txtools::fetch_transaction;
 pub(crate) use crate::persist::connector::CoreDatabaseConnector;
 
@@ -121,91 +117,3 @@ impl CoreDatabaseConnector for EthereumConnector {
             .collect()
     }
 }
-
-
-#[derive(Debug, Error)]
-pub enum SubmitPaymentTxnError {
-    #[error("Internal query error occurred: {0:?}")]
-    QueryError(#[from] QueryError),
-
-    #[error("User is not registered yet!")]
-    UserNotRegistered,
-
-    #[error("Not enough deposit available!")]
-    NotEnoughDeposit,
-
-    #[error("Found conflicting transactions!")]
-    ConflictingTransactions,
-}
-
-pub async fn submit_payment_transaction(
-    ctx: &PersistCtx,
-    user_addr: String,
-    recipient_address: String,
-    transaction_id: String,
-    amount: f64,
-    cert: String,
-) -> Result<(), SubmitPaymentTxnError> {
-    ctx.client
-        ._transaction()
-        .run::<SubmitPaymentTxnError, _, _, _>(|client| async move {
-            let Some(user) = client
-                .user()
-                .find_unique(user::address::equals(user_addr.clone()))
-                // Fetching the user's not-yet-finalized transactions
-                .with(user::transactions::fetch(vec![
-                    user_transaction::finalized::equals(false),
-                    user_transaction::tx_id::not(transaction_id.clone()),
-                ]))
-                .exec()
-                .await?
-            else {
-                return Err(SubmitPaymentTxnError::UserNotRegistered);
-            };
-
-            let transactions = user.transactions.unwrap();
-            let not_usable_deposit = transactions.iter().map(|tx| tx.amount).sum::<f64>();
-
-            if not_usable_deposit + amount > user.deposit {
-                return Err(SubmitPaymentTxnError::NotEnoughDeposit);
-            }
-
-            client
-                .user_transaction()
-                .upsert(
-                    user_transaction::tx_id::equals(transaction_id.clone()),
-                    user_transaction::create(
-                        transaction_id,
-                        recipient_address,
-                        amount,
-                        user::address::equals(user_addr.clone()),
-                        vec![user_transaction::cert::set(Some(cert))],
-                    ),
-                    vec![],
-                )
-                .exec()
-                .await?;
-
-            // For now, we implement the optimistic lock strategy using the user's version, because prisma
-            //  doesn't support SELECT FOR UPDATE yet.
-            let updated_user = client
-                .user()
-                .update(
-                    user::address::equals(user_addr),
-                    vec![user::version::increment(1)],
-                )
-                .exec()
-                .await?;
-
-            // There was a conflicting version, so we return error here...
-            if updated_user.version != user.version + 1 {
-                return Err(SubmitPaymentTxnError::ConflictingTransactions);
-            }
-
-            Ok(())
-        })
-        .await?;
-
-    Ok(())
-}
-
