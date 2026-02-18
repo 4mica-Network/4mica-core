462cb9b000e31e07423ba18bf21a91a959ef7e4d
Mairon <mairon.mahzoun@gmail.com>
2026-02-18 10:31:45 +0100

    fix(core): allow remunerate while paused

--- contracts/test/Core4MicaPausable.t.sol
+++ contracts/test/Core4MicaPausable.t.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.29;
 
 import "./Core4MicaTestBase.sol";
+import {Guarantee} from "../src/Core4Mica.sol";
+import {BLS} from "@solady/src/utils/ext/ithaca/BLS.sol";
 
 error EnforcedPause();
 
@@ -42,6 +44,34 @@ contract Core4MicaPausableTest is Core4MicaTestBase {
         core4Mica.deposit{value: 1 ether}();
         (uint256 collateralAfter, , ) = core4Mica.getUser(USER1);
         assertEq(collateralAfter, collateralBefore + 1 ether);
+    }
+
+    function test_Pause_DoesNotBlockRemuneration() public {
+        vm.prank(USER1);
+        core4Mica.deposit{value: 1 ether}();
+
+        uint256 tabId = 0x1234;
+        uint256 reqId = 17;
+        uint256 tabTimestamp = 1;
+        vm.warp(tabTimestamp + core4Mica.remunerationGracePeriod() + 5);
+
+        Guarantee memory g = _ethGuarantee(
+            tabId,
+            tabTimestamp,
+            USER1,
+            USER2,
+            reqId,
+            0.5 ether
+        );
+        BLS.G2Point memory signature = _signGuarantee(g, TEST_PRIVATE_KEY);
+        bytes memory guaranteeData = _encodeGuaranteeWithVersion(g);
+
+        core4Mica.pause();
+
+        uint256 user2BalanceBefore = USER2.balance;
+        vm.prank(USER2);
+        core4Mica.remunerate(guaranteeData, signature);
+        assertEq(USER2.balance, user2BalanceBefore + 0.5 ether);
     }
 
     function test_Pause_Revert_UnauthorizedCallers() public {
